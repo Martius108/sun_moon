85bedcdec9ce80dd4069bc3a432d21c1672a07a7
Martin lanius <martin.lanius@icloud.com>
2025-03-18 08:49:53 +0100

    Initial Commit

---
+++ Sun&Moon/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Sun&Moon
+//
+//  Created by Martin Lanius on 16.03.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Sun&Moon/Sun_MoonApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sun_MoonApp.swift
+//  Sun&Moon
+//
+//  Created by Martin Lanius on 16.03.25.
+//
+
+import SwiftUI
+
+@main
+struct Sun_MoonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
